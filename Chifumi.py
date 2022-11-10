d071e8e1f588b77f419d4401d2f03b27baff3976
Gwenitora <gwenitora.gardu74@gmail.com>
2022-11-10 16:59:09 +0100

    feature: start

---
+++ Chifumi.py
@@ -0,0 +1,8 @@
+# J'admet que la fonction input(str) avec comme paramètre facultatif str permet à l'utilisateur de renvoyer un string en retour de l'execution de cette fonction, str permet d'afficher un message devant l'interaction du joueur
+# J'admet que la fonction randint(x, y) avec comme paramètre x et y permet de renvoyerun entier naturel aléatoire compris entre x compris et y compris  en retour de l'execution de cette fonction
+
+# Définir la fonction question avec comme paramètres un string nommé text une liste nommé table
+    # Tant que le retour de l'execution de la fonction input avec comme paramètre text, est compris entre 0 et 
+
+# Définir la fonction play
+    # 
